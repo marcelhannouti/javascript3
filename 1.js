a8b275c01f95da4689d7ec231c034f6197ae09c5
marc <marc.hannouti360@gmail.com>
2022-08-10 10:57:22 +0200

    commit

---
+++ 1.js
@@ -0,0 +1,32 @@
+let nombre="marc"
+let numero =123
+let boo = true
+let und
+let nul = null
+var numero1 = 4666
+let array = [1,6,"cdsvsvs"] 
+let algo= {
+    nombre:"sv",
+    apellido:"ssvs",
+    ciudad:"cdscd"
+}
+const valor = Symbol ("cda")
+console.log(nombre)
+console.log(typeof(nombre))
+console.log(numero)
+console.log(typeof(numero))
+console.log(boo)
+console.log(typeof(boo))
+console.log(und)
+console.log(typeof(und))
+console.log(nul)
+console.log(typeof(nul))
+console.log(array)
+console.log(typeof(array))
+console.log(algo)
+console.log(typeof(algo))
+console.log(valor)
+console.log(typeof(valor))
+
+
+
